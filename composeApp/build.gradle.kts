c8504b6379d65d7d1e61b43ecccd97bf2feda137
Naoki-1987 <1987n.iwaoka@gmail.com>
2026-05-28 23:21:15 -0500

    Update App Icon

--- composeApp/build.gradle.kts
+++ composeApp/build.gradle.kts
@@ -51,6 +51,19 @@ android {
         versionCode = 1
         versionName = "1.0"
     }
+    
+    buildTypes {
+        getByName("release") {
+            isMinifyEnabled = true
+            isShrinkResources = true
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+            signingConfig = signingConfigs.getByName("debug")
+        }
+    }
+
     packaging {
         resources {
             excludes += "/META-INF/{AL2.0,LGPL2.1}"
